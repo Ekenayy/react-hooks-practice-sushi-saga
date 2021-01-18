f494e171c65beecd2523d4268a52cb8c5ad944b7
Ekenayy <ekenayy@gmail.com>
2021-01-18 13:23:00 -0500

    commit tree 256978623cf706237e69bc131823d07754251dea

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+  // _limit=4&offset=8
+  const [sushis, setSushis] = useState([])
+  const [boughtSushi, setBoughtSushi] = useState([])
+  const [money, setMoney] = useState(100)
+
+  const onBuy = (sushi) => {
+
+    setBoughtSushi([
+      ...boughtSushi,
+      sushi
+    ])
+    setMoney(money - sushi.price)
+  
+  }
+
+  // console.log(money)
+
+  useEffect(() => {
+    fetch(API)
+      .then(response => response.json())
+      .then(sushis => setSushis(sushis))
+  } 
+  ,[])
+
   return (
     <div className="app">
-      <SushiContainer />
-      <Table />
+      <SushiContainer money={money} onBuy={onBuy} sushis={sushis} boughtSushi={boughtSushi} setBoughtSushi={setBoughtSushi} />
+      <Table boughtSushi={boughtSushi} money={money} />
     </div>
   );
 }
